80aa398b72f297ac3353a6ee5f0536b85a8b6c3a
xiajun <913492952@qq.com>
2017-04-19 13:31:03 +0800

    添加获取视频时长方法

--- LibVideoRecord/src/main/java/com/style/lib/media/video/PlayVideoActivity.java
+++ LibVideoRecord/src/main/java/com/style/lib/media/video/PlayVideoActivity.java
@@ -67,4 +67,30 @@ public class PlayVideoActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void getVideoDuration(String path){
+        android.media.MediaMetadataRetriever mmr = new android.media.MediaMetadataRetriever();
+
+        try {
+            if (mUri != null) {
+                HashMap<String, String> headers = mHeaders;
+                if (headers == null) {
+                    headers = new HashMap<String, String>();
+                    headers.put("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-CN; MW-KW-001 Build/JRO03C) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 UCBrowser/1.0.0.001 U4/0.8.0 Mobile Safari/533.1");
+                }
+                mmr.setDataSource(mUri, headers);
+            } else {
+                mmr.setDataSource(mFD, mOffset, mLength);
+            }
+
+            String duration = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_DURATION);
+            String width = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+            String height = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+
+        } catch (Exception ex) {
+            Log.e(TAG, "MediaMetadataRetriever exception " + ex);
+        } finally {
+            mmr.release();
+        }
+    }
 }

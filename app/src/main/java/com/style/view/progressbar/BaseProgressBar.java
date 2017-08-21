6c0723e065927693f8af2f9b282795df34fcdfd7
xiajun <913492952@qq.com>
2017-08-21 16:45:15 +0800

    添加自定义view2

---
+++ app/src/main/java/com/style/view/progressbar/BaseProgressBar.java
@@ -0,0 +1,94 @@
+package com.style.view.progressbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by xiajun on 2017/7/31.
+ */
+
+public abstract class BaseProgressBar extends View {
+    private static final String TAG = "BaseProgressBar";
+
+    //进度
+    protected int progress = 0;
+    private PercentThread percentThread;
+
+    public BaseProgressBar(Context context) {
+        super(context);
+    }
+    public BaseProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    //带动画渐进到指定进度
+    public void setPercentWithAnimation(int percent) {
+        if (percent > 100) {
+            throw new IllegalArgumentException("percent must less than 100!");
+        }
+        if (percentThread != null && percentThread.isAlive()) {
+            //停止上一次绘制线程，否则之前可能还在绘制
+            percentThread.setStop();
+            percentThread.interrupt();
+        }
+        percentThread = new PercentThread(percent);
+        percentThread.setPercent(percent);
+        percentThread.start();
+    }
+
+    private class PercentThread extends Thread {
+        private int percent;
+        private boolean canContinue = true;
+
+        public PercentThread(int percent) {
+            this.percent = percent;
+        }
+
+        public void setPercent(int percent) {
+            this.percent = percent;
+        }
+
+        @Override
+        public void run() {
+            int sleepTime = 1;
+            for (int i = 0; i <= percent; i++) {
+                if (canContinue) {
+                    if (i % 20 == 0) {
+                        sleepTime += 2;
+                    }
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    progress = i;
+                    postInvalidate();
+                }
+            }
+        }
+
+        public void setStop() {
+            canContinue = false;
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Log.e(TAG, "onAttachedToWindow");
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Log.e(TAG, "onDetachedFromWindow");
+        if (percentThread != null && percentThread.isAlive()) {
+            percentThread.interrupt();
+        }
+        percentThread = null;
+        System.gc();
+    }
+}
